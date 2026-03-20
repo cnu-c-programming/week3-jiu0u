5ce59b2014302535ca2d626eebd869391be48e49
jiyu <pllpopogg@gmail.com>
2026-03-20 10:38:52 +0900

    3-4

---
+++ code/3-4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int max_of(int count, ...) {
+    va_list ap;
+    va_start(ap, count);
+    int max = va_arg(ap, int);
+    for (int i = 0; i < count-1 ; i++) {
+        int num = va_arg(ap, int);
+        if (num > max) {
+            max = num;
+        }
+    }
+    va_end(ap);
+    return max;
+}
+
+int main()
+{
+    printf("%d\n", max_of(4, 5, 12, 3, 9)); 
+    printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6)); 
+    printf("%d\n", max_of(3, 100, 200, 300));
+
+    return 0;
+}

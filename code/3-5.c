b7b6c12d9f2c0b0666b9799cb93a52364fe8567c
jiyu <pllpopogg@gmail.com>
2026-03-20 10:52:08 +0900

    3-5

---
+++ code/3-5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void my_sum(char type, int count, ...) {
+    va_list ap;
+    va_start(ap, count);
+    if (type == 'S') {
+        for (int i = 0; i < count; i++) {
+            printf("%s ", va_arg(ap, char*));
+        }
+    }
+    else if (type == 'C') {
+        for (int i = 0; i < count; i++) {
+            printf("%c", va_arg(ap, char));
+        }
+    }
+    else if (type == 'D') {
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            sum += va_arg(ap, int);
+        }
+        printf("%d", sum);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U'); 
+    my_sum('D', 4, 10, 20, 30, 40);
+
+    return 0;
+}
